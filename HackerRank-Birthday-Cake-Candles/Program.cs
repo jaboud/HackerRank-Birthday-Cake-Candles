ada1412cd829984384049346bfab52aedda128e7
jaboud <jaboud94@gmail.com>
2019-09-25 16:45:34 +1000

    Created iniital project solution

---
+++ HackerRank-Birthday-Cake-Candles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank_Birthday_Cake_Candles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
